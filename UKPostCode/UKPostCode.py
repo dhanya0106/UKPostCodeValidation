7d80768128ae354b152bc368b0981b68c9905a3b
dhanya0106 <40861780+dhanya0106@users.noreply.github.com>
2018-07-16 18:25:12 +0100

    Update UKPostCode.py

--- UKPostCode/UKPostCode.py
+++ UKPostCode/UKPostCode.py
@@ -1,38 +1,40 @@
 import re
 
+PATTERN = '^([A-PR-UWYZ][A-HK-Y]?)' \
+          '([\d]{1})' \
+          '([\dA-Z]?)' \
+          '([\d]{1}[ABD-HJLNPQ-Z]{2})$'
+
 
 class UKPostCode(object):
     def __init__(self):
         self.post_code = {}
 
-    def __extract_validate(self, s):
+    def __extract(self, s):
         s = s.strip()
         # Capture Area code, District code and Inward code respectively
-        PATTERN = '^([A-PR-UWYZ][A-HK-Y]?)' \
-                  '([\d]{1})' \
-                  '([\dA-Z]?)' \
-                  '([\d]{1}[ABD-HJLNPQ-Z]{2})$'
         codes = re.match(PATTERN, s)
         if codes is None:
             return False
-        # Extract
         parts = list(codes.groups())
         optional_char = parts.pop(2)
         self.post_code['area_code'] = parts[0]
         self.post_code['district_code'] = parts[1]
         self.post_code['inward_code'] = parts[2]
+        res = self.__validate(parts, optional_char)
+        return res
         
-        # Validation
+    def __validate(self, parts, optional_char):
         if None in parts:
             return False
         else:
             if optional_char is not None:
                 if optional_char.isalpha():
-                    if len(self.post_code['area_code']) == 1:
-                        if not optional_char in 'ABCDEFGHJKPSTUW':
+                    if len(self.post_code['area_code']) == 1\
+                    and optional_char not in 'ABCDEFGHJKPSTUW':
                             return False
-                    elif len(self.post_code['area_code']) == 2:
-                        if not optional_char in 'ABEHMNPRVWXY':
+                    elif len(self.post_code['area_code']) == 2\
+                    and optional_char not in 'ABEHMNPRVWXY':
                             return False
                     else:
                         return False
@@ -58,19 +60,23 @@ class UKPostCode(object):
     def validate_postcode(self, s):
         """
         :param s: String to be validated
-        :return: Formatted code if valid, exception if not valid
+        :return: Boolean value
         """
         s = s.strip().upper()
         s = re.sub('\s','',s)
         is_special_case = self.__validate_special_case(s)
         if is_special_case:
             return True
-        is_valid_case = self.__extract_validate(s)
+        is_valid_case = self.__extract(s)
         if is_valid_case:
             return True
         return False
 
     def format_postcode(self, s):
+        """
+        :param s: String to be formatted
+        :return: Formatted code if valid, exception if not valid
+        """
         if self.validate_postcode(s):
             return self.__format_code(self.post_code)
         else:
